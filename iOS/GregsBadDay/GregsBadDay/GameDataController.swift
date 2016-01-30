fb48d6e720e84440a81641fd7690582534f2980f
Chris Weathers <chris@doubleencore.com>
2016-01-30 10:06:54 -0700

    Added unit tests.  In a game jam project?!

--- iOS/GregsBadDay/GregsBadDay/GameDataController.swift
+++ iOS/GregsBadDay/GregsBadDay/GameDataController.swift
@@ -10,4 +10,14 @@ class GameDataController {
     var name: String?
     var room: String?
     var team: String?
+    
+    var level: Int?
+    
+    func update() {
+        
+    }
+    
+    func makeRequest() {
+        
+    }
 }
