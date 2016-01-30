fe9e01e98520d31b0e16e1b115f639d17c2e6fd5
nickdobos <ndobos@doubleencore.com>
2016-01-30 14:44:01 -0700

    Setup for easy walk swapping

--- iOS/GregsBadDay/GregsBadDay/GregsDayScene.swift
+++ iOS/GregsBadDay/GregsBadDay/GregsDayScene.swift
@@ -8,69 +8,120 @@
 
 import SpriteKit
 
+enum GregWalkType {
+    case Normal
+    case Moonwalk
+    
+    case Count
+    static let allValues = [Normal, Moonwalk, Count]
+}
+
 class GregsDayScene: SKScene {
     
-    var walkingFrames = [SKTexture]()
     var greg = SKSpriteNode()
+
+    var walkingFrames = [GregWalkType :[SKTexture]]()
+    var defaultStandingFrame = SKTexture()
+    
+    var currentWalkType = GregWalkType.Normal
+    var movingRight = true
     
     override func didMoveToView(view: SKView) {
-        let walkCycle = SKTextureAtlas(named: "ShittyWalkCycle")
+        loadWalkCycle(.Normal)
+        loadWalkCycle(.Moonwalk)
         
-        for var i = 0; i < walkCycle.textureNames.count; i++ {
-            let name = String(format: "ShittyWalkCycle_000%02d.png", i)
-            let texture = walkCycle.textureNamed(name)
-            walkingFrames.append(texture)
-        }
+        // TODOAdd More walk cycles here
         
-        greg = SKSpriteNode(texture: walkingFrames[0])
+        
+        defaultStandingFrame = walkingFrames[.Normal]![0]
+        greg = SKSpriteNode(texture: defaultStandingFrame)
+        
         addChild(greg)
         greg.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
         
-        gregWalk()
+        gregWalkWithType(.Normal)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        /* Called when a touch begins */
-//        for touch in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
+        let indexOfNextEnum = GregWalkType.allValues.indexOf(currentWalkType)! + 1
+        
+        var newWalkType = GregWalkType.allValues[indexOfNextEnum]
+        if newWalkType == .Count {
+          newWalkType = .Normal
+        }
+        
+        setNewWalkType(newWalkType)
     }
     
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
     
-    func gregWalk() {
-        let xToMoveTo = greg.xScale > 0 ? 0 : self.frame.size.width
+    func setNewWalkType(type: GregWalkType) {
+        greg.removeAllActions()
+        
+        let wait = SKAction.waitForDuration(1)
+        let flip = SKAction.scaleXTo(greg.xScale * -1, duration: 0.5)
+        let flipBack = SKAction.scaleXTo(greg.xScale, duration: 0.25)
+        let moveAgain = SKAction.runBlock { () -> Void in
+            self.gregWalkWithType(type)
+        }
+        
+        let sequence = SKAction.sequence([wait, flip, flipBack, moveAgain])
+        
+        greg.runAction(sequence, withKey: "new walk")
+    }
+    
+    func gregWalkWithType(type : GregWalkType) {
+        let xToMoveTo = movingRight ? self.frame.size.width : 0
+        
+        if (type == .Moonwalk) {
+            greg.xScale = movingRight ? -fabs(greg.xScale) : fabs(greg.xScale);
+        }
         
         let moveAction = SKAction.moveTo(CGPoint(x: xToMoveTo, y: self.frame.size.height/2), duration: 5)
         let doneAction = SKAction.runBlock { () -> Void in
-            self.walkEnded()
+            self.walkEnded(type)
         }
         
         let motionAction = SKAction.sequence([moveAction, doneAction])
         greg.runAction(motionAction, withKey: "moving")
         
-        greg.runAction((SKAction.repeatActionForever(SKAction.animateWithTextures(walkingFrames, timePerFrame: 0.1, resize: false, restore: true))), withKey: "walking")
+        greg.runAction((SKAction.repeatActionForever(SKAction.animateWithTextures(walkingFrames[type]!, timePerFrame: 0.1, resize: false, restore: true))), withKey: "walking")
+        
+        currentWalkType = type
     }
     
-    func walkEnded() {
+    func walkEnded(type : GregWalkType) {
         greg.removeAllActions()
         
-        greg.xScale = greg.xScale * -1
+        movingRight = !movingRight
         
-        gregWalk()
+        gregWalkWithType(type)
+    }
+    
+    func loadWalkCycle(type: GregWalkType) {
+        var atlas = SKTextureAtlas()
+        var imageName = ""
+        
+        switch (type) {
+        case .Normal, .Moonwalk:
+            atlas = SKTextureAtlas(named: "ShittyWalkCycle")
+            imageName = "ShittyWalkCycle_000%02d.png"
+            
+            break
+        default:
+            return
+        }
+        
+        var frames = [SKTexture]()
+        
+        for var i = 0; i < atlas.textureNames.count; i++ {
+            let name = String(format: imageName, i)
+            let texture = atlas.textureNamed(name)
+            frames.append(texture)
+        }
+        
+        walkingFrames[type] = frames
     }
 }
