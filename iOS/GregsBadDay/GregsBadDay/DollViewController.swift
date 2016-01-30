4dc08a0b8bd3bd754a6a0c24ec7f7746929cf2a5
Chris Weathers <chris@doubleencore.com>
2016-01-30 15:35:22 -0700

    Progress in DollViewController.

--- iOS/GregsBadDay/GregsBadDay/DollViewController.swift
+++ iOS/GregsBadDay/GregsBadDay/DollViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 
 class DollViewController: UIViewController {
+    
+    @IBOutlet var armsButton: UIButton?
+    @IBOutlet var bodyButton: UIButton?
+    @IBOutlet var headButton: UIButton?
+    @IBOutlet var legsButton: UIButton?
+    
+    @IBOutlet var damageSlider: UISlider?
+    
+    @IBOutlet var sideSwitch: UISwitch?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func armsButtonTapped() {
+        performActionOnTarget(PlayerAction.Target.Arms)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func bodyButtonTapped() {
+        performActionOnTarget(PlayerAction.Target.Body)
     }
-    */
+    
+    @IBAction func headButtonTapped() {
+        performActionOnTarget(PlayerAction.Target.Head)
+    }
+    
+    @IBAction func legsButtonTapped() {
+        performActionOnTarget(PlayerAction.Target.Legs)
+    }
+    
+    func performActionOnTarget(target:PlayerAction.Target) {
+        
+    }
 
 }
