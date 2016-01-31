5794bcdec6d073e5a42f428c4ea4e8f65f2a48cf
Chris Weathers <chris@doubleencore.com>
2016-01-31 01:23:29 -0700

    Safety overhaul in GameDataController to hopefully avoid crashes.

--- iOS/GregsBadDay/GregsBadDay/GameDataController.swift
+++ iOS/GregsBadDay/GregsBadDay/GameDataController.swift
@@ -11,28 +11,29 @@ import Foundation
 class GameDataController: NSObject {
     var roundResult: RoundResult?
     
-    func postRequestForPlayerAction(playerAction:PlayerAction, completionHandler:(roundResult:RoundResult)-> Void) {
+    func postRequestForPlayerAction(playerAction:PlayerAction, completionHandler:(roundResult:RoundResult?)-> Void) {
         
         let request = requestForPlayerAction(playerAction)
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
-            let roundResult = self.roundResultForData(data!)
-            self.roundResult = roundResult
-            completionHandler(roundResult: roundResult!)
+
+            if let data = data {
+                let roundResult = self.roundResultForData(data)
+                self.roundResult = roundResult
+                completionHandler(roundResult: roundResult)
+            }
+            else {
+                completionHandler(roundResult: nil)
+            }
         }
         task.resume()
     }
     
-    func bodyDataForPlayerAction(playerAction:PlayerAction)-> NSData? {
-
-        do {
-            let bodyDictionary = bodyDictionaryForPlayerAction(playerAction)
-            let bodyData = try NSJSONSerialization.dataWithJSONObject(bodyDictionary, options:[])
-            return bodyData
-        }
-        catch {
-            return nil
-        }
+    func bodyDataForPlayerAction(playerAction:PlayerAction)-> NSData {
+        
+        let bodyDictionary = bodyDictionaryForPlayerAction(playerAction)
+        let bodyData = try! NSJSONSerialization.dataWithJSONObject(bodyDictionary, options:[])
+        return bodyData
     }
     
     func bodyDictionaryForPlayerAction(playerAction:PlayerAction)-> [String: AnyObject] {
@@ -64,37 +65,28 @@ class GameDataController: NSObject {
         return request
     }
     
-    func roundResultDictionaryForData(data:NSData)-> [String: AnyObject]? {
-        do {
-            let dictionary = try NSJSONSerialization.JSONObjectWithData(data, options:[]) as! [String: AnyObject]
-            return dictionary
-        }
-        catch {
-            return nil
-        }
+    func roundResultDictionaryForData(data:NSData)-> [String: AnyObject] {
+        let dictionary = try! NSJSONSerialization.JSONObjectWithData(data, options:[]) as! [String: AnyObject]
+        return dictionary
     }
     
-    func roundResultForData(data:NSData)-> RoundResult? {
+    func roundResultForData(data:NSData)-> RoundResult {
         
-        if let dictionary = roundResultDictionaryForData(data) {
+        var dictionary = roundResultDictionaryForData(data)
+        
+        var roundResult = RoundResult()
+        
+        roundResult.bodyValue = (dictionary["body_score"] as? Int) ?? 0
+        roundResult.headValue = (dictionary["head_score"] as? Int) ?? 0
+        roundResult.leftArmValue = (dictionary["arm_left_score"] as? Int) ?? 0
+        roundResult.leftLegValue = (dictionary["leg_left_score"] as? Int) ?? 0
+        roundResult.rightArmValue = (dictionary["arm_right_score"] as? Int) ?? 0
+        roundResult.rightLegValue = (dictionary["leg_right_score"] as? Int) ?? 0
+        
+        roundResult.currentRound = (dictionary["current_level"] as? Int) ?? 0
+        roundResult.nextRound = dictionary["next_level"] as? Int
             
-            var roundResult = RoundResult()
-            
-            roundResult.bodyValue = dictionary["body_score"] as! Int
-            roundResult.headValue = dictionary["head_score"] as! Int
-            roundResult.leftArmValue = dictionary["arm_left_score"] as! Int
-            roundResult.leftLegValue = dictionary["leg_left_score"] as! Int
-            roundResult.rightArmValue = dictionary["arm_right_score"] as! Int
-            roundResult.rightLegValue = dictionary["leg_right_score"] as! Int
-            
-            roundResult.currentRound = dictionary["current_level"] as! Int
-            roundResult.nextRound = dictionary["next_level"] as? Int
-            
-            return roundResult
-        }
-        else {
-            return nil
-        }
+        return roundResult
     }
     
 }
