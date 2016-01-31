9e85fa51544102a5d801f6a991237bb4290b8373
Chris Weathers <chris@doubleencore.com>
2016-01-30 23:37:43 -0700

    Response fix.

--- iOS/GregsBadDay/GregsBadDay/GameDataController.swift
+++ iOS/GregsBadDay/GregsBadDay/GameDataController.swift
@@ -81,9 +81,9 @@ class GameDataController: NSObject {
             
             roundResult.bodyValue = dictionary["body_score"] as! Int
             roundResult.headValue = dictionary["head_score"] as! Int
-            roundResult.leftArmValue = dictionary["arms_score"] as! Int
-            roundResult.legsValue = dictionary["legs_score"] as! Int
-            roundResult.rightArmValue = dictionary["arms_score"] as! Int
+            roundResult.leftArmValue = dictionary["arm_left_score"] as! Int
+            roundResult.legsValue = dictionary["leg_left_score"] as! Int
+            roundResult.rightArmValue = dictionary["arm_right_score"] as! Int
             
             roundResult.currentRound = dictionary["current_level"] as! Int
             roundResult.nextRound = dictionary["next_level"] as? Int
